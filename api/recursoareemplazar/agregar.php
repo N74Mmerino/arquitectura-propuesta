4b66b1a691136806f956ef22652e0f44239d5a23
N74Mmerino <nnicolasmmerino@gmail.com>
2024-10-04 20:31:23 -0300

    2da version- Arquitectura

--- api/recursoareemplazar/agregar.php
+++ api/recursoareemplazar/agregar.php
@@ -3,7 +3,12 @@
 header('Content-Type: application/json');
 
 require_once 'modelosRespuestas/agregarRespuesta.php';
- 
+require_once 'modelosRequest/agregarRequest.php';
+
+//se obtiene el body
+$json = file_get_contents('php://input',true);
+// Convertir el body en un objeto
+$req = json_decode($json);
 
 $resp= new AgregarRespuesta();
 
